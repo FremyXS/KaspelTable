e85a8c1e3241bb016cb6850b12cec410c68c1736
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-31 01:42:25 +0500

    feat: add useRef

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,11 +1,27 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
 
 import './ModalWindow.scss';
 
 function ModalWindow({ children, onClick }: { children: ReactNode, onClick: () => void }) {
+    const elementRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (elementRef.current && !elementRef.current.contains(event.target as Node)) {
+                onClick();
+            }
+        };
+
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
+
     return (
         <div className="modal-window">
-            <div className="modal-window-form">
+            <div className="modal-window-form" ref={elementRef}>
                 <div className="modal-window-form__head">
                     <p>Kaspel</p>
                     <button type="button" onClick={onClick}>Exit</button>
