a514f0476352342bd3cb871cf200bea057a11c67
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-31 00:39:26 +0500

    feat: add mock

---
+++ src/components/Table/mocks/index.ts
@@ -0,0 +1,94 @@
+import { DataType } from "../../../types";
+
+export const dataMock: DataType[] = [
+    {
+        key: 0,
+        name: 'Максим Пятый',
+        date: '2023-03-28',
+        num: 934,
+    },
+    {
+        key: 1,
+        name: 'John Doe',
+        date: '2022-12-15',
+        num: 450,
+    },
+    {
+        key: 2,
+        name: 'Jane Smith',
+        date: '2022-11-02',
+        num: 789,
+    },
+    {
+        key: 3,
+        name: 'Alex Johnson',
+        date: '2022-08-25',
+        num: 123,
+    },
+    {
+        key: 4,
+        name: 'Sarah Wilson',
+        date: '2022-05-18',
+        num: 987,
+    },
+    {
+        key: 5,
+        name: 'David Brown',
+        date: '2022-02-11',
+        num: 654,
+    },
+    {
+        key: 6,
+        name: 'Emily Taylor',
+        date: '2021-11-04',
+        num: 321,
+    },
+    {
+        key: 7,
+        name: 'Michael Davis',
+        date: '2021-08-28',
+        num: 567,
+    },
+    {
+        key: 8,
+        name: 'Jessica Miller',
+        date: '2021-05-21',
+        num: 890,
+    },
+    {
+        key: 9,
+        name: 'Christopher Anderson',
+        date: '2021-02-12',
+        num: 432,
+    },
+    {
+        key: 10,
+        name: 'Sophia Garcia',
+        date: '2020-11-05',
+        num: 765,
+    },
+    {
+        key: 11,
+        name: 'William Wilson',
+        date: '2020-08-29',
+        num: 109,
+    },
+    {
+        key: 12,
+        name: 'Olivia Thompson',
+        date: '2020-05-22',
+        num: 543,
+    },
+    {
+        key: 13,
+        name: 'Ryan Moore',
+        date: '2020-02-13',
+        num: 876,
+    },
+    {
+        key: 14,
+        name: 'Ava Thomas',
+        date: '2019-11-06',
+        num: 210,
+    },
+]
