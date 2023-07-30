b52f5a24bcb797446d73cbe07ac3df2fc4955ba4
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-30 12:02:44 +0500

    refactor: add upload new data function

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,61 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import Table from './components/Table/Table';
 import ModalWindow from './components/ModalWindow/ModalWindow';
 import Input from './components/Input/Input';
+import Button from './components/Button/Button';
+import { DataType } from './types';
 
 import './App.css';
-import Button from './components/Button/Button';
 
 function App() {
   const [isShowWindow, setIsShowWindow] = useState<boolean>();
+  const [data, setData] = useState<DataType[]>([]);
+  const [formData, setFormData] = useState<DataType>({
+    name: "",
+    date: "",
+    num: 0,
+  });
+
+  const handleAddDataClick = () => {
+    setData([...data, formData]);
+    setFormData({
+      name: "",
+      date: "",
+      num: 0,
+    });
+    setIsShowWindow(false);
+  }
+
+  const onChangeForm = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
 
   return (
     <div className="App">
       {isShowWindow &&
         <ModalWindow onClick={() => setIsShowWindow(false)}>
-          <Input labelName='Name' />
-          <Input labelName='Date' />
-          <Input labelName='Num' />
-          <Button type='button'>
+          <Input labelName='Name'
+            type='text'
+            name='name'
+            value={formData.name}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => onChangeForm(event)} />
+          <Input labelName='Date'
+            type='date'
+            name='date'
+            value={formData.date}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => onChangeForm(event)} />
+          <Input labelName='Num'
+            type='number'
+            name='num'
+            value={formData.num}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => onChangeForm(event)} />
+          <Button type='button'
+            onClick={() => handleAddDataClick()}>
             Add
           </Button>
         </ModalWindow>
